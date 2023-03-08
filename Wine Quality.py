0f1bf245fbf9253811860a89ebc281e21a0949a5
Otavio Muraca <42158209+otaviomuraca@users.noreply.github.com>
2023-03-07 23:12:11 -0500

    Create Wine Quality.py

---
+++ Wine Quality.py
@@ -0,0 +1,39 @@
+# Módulo 1: Fundamentos de Aprendizado de Máquina
+# Esse dataset contém um conjunto de atributos (dados de sensores) sobre o processo de fabricação de vinhos (tinto e branco). 
+# Esses dados são utilizados para classificar, ao final do processo, a qualidade do vinho obtido. Existem informações como o teor alcoólico e nível de acidez.
+
+#Importações de Bibliotecas
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy
+import pydotplus
+
+from scipy.optimize import curve_fit
+
+from sklearn import neighbors, datasets, metrics
+from sklearn.svm import SVC
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, precision_scor
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+
+from mlxtend.plotting import plot_confusion_matrix
+
+from six import StringIO
+
+from IPython.display import Image
+
+#################################
+
+#Importação do DATASET
+
+data = pd.read_csv('/data/winequality-red.csv', sep=';')
+
+data
+
